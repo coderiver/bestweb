8c4349b83cc143869bb8f829d35703a3b955e503
whats0n <mrdvv@mail.ru>
2015-04-08 13:53:59 +0300

    novaplat

--- js/common.js
+++ js/common.js
@@ -1,126 +1,62 @@
-head.ready(function() {
+head.ready(function()
+{
 	//select
+	$(document).ready(function()
+	{
+        //input mask
+        $.extend($.inputmask.defaults, {
+            'autoUnmask': true
+        });
 
-	//popups
-	$('.js-send').on('click', function() {
-		$('.js-add-pop').addClass('js-open');
-		$('.form__box').addClass('js-close');
-		return false;
+        //$('.mb_amount').inputmask("decimal", {
+        //    groupSeparator: " ",
+        //    digits: 0,
+        //    autoGroup: true,
+        //    rightAlign: false,
+        //});
+
+        $('.mb_phone').inputmask('+9{11,14}', {
+            onUnMask: function(maskedValue, unmaskedValue) {
+                return '+' + unmaskedValue;
+            }
+        });
+
+        $('.mb_phone_ru').inputmask("+7 (999) 999 9999", {
+            onUnMask: function(maskedValue, unmaskedValue) {
+                return '+7' + unmaskedValue;
+            }
+        });
+
+        $('.mb_card_number').inputmask("9999 9999 9999 9999");
+
+        $('.mb_date').inputmask({
+            mask: "1.2.y",
+            placeholder: "ДД.ММ.ГГГГ",
+            leapday: "29.02.",
+            separator: '.',
+            alias: "dd/mm/yyyy"
+        });
+
+        $(".mb_address").suggestions({
+            serviceUrl: "https://dadata.ru/api/v2",
+            token: "8799b46a84b5e9746a84235ba02fe11364c5fa7b",
+            type: "ADDRESS"
+        });
+
+        $(".mb_fio").suggestions({
+            serviceUrl: "https://dadata.ru/api/v2",
+            token: "8799b46a84b5e9746a84235ba02fe11364c5fa7b",
+            type: "NAME"
+        });
+
+        initJSLists();
+
 	});
-	$('.js-follow').on('click', function() {
-		$('.js-follow-pop').addClass('js-open');
-		$('.form__box').addClass('js-close');
-		return false;
-	});
-	$('.js-transfer').on('click', function() {
-		$('.steps-first').addClass('js-open');
-		$('.form__box').addClass('js-close');
-		return false;
-	});
-	$('.js-add').on('click', function() {
-		$('.js-send-pop').addClass('js-open');
-		$('.js-add-pop').removeClass('js-open');
-		$('.form__box').addClass('js-close');
-		return false;
-	});
-	//tabs
-	$('.steps__list li a').click(function(event) {
-		/* Act on the event */
-		attr = $(this).attr('href');
-		$('#steps-first,#steps-second,#steps-third,#steps-fourth,#steps-fifth').removeClass('js-open');
-		$(' '+attr).addClass('js-open');
-		return false;
-	});
-	$('.js-next-step').click(function(event) {
-		/* Act on the event */
-		attr = $(this).attr('href');
-		$('#steps-first,#steps-second,#steps-third,#steps-fourth,#steps-fifth').removeClass('js-open');
-		$(' '+attr).addClass('js-open');
-		return false;
-	});
-	$('.close').on('click', function() {
-		$('.popup').removeClass('js-open');
-		$('.steps').removeClass('js-open');
-		$('.form__box').removeClass('js-close');
-		return false;
-	});
-	$(document).ready(function() {
-	    $(document).click(function() {
-	        $(".js-select-list").hide();
-	        $(".js-select").removeClass("is-active");
-	    });
-	    function selectList() {
-	        var select = $(".js-select");
-	        var select_list = $(".js-select-list");
-	        $("body").on("click", ".js-select", function(event){
-	            if ($(this).hasClass("is-active")) {
-	                select.removeClass("is-active");
-	                select_list.hide();
-	            }
-	            else {
-	                select.removeClass("is-active");
-	                select_list.hide();
-	                $(this).find(".js-select-list").show();
-	                $(this).addClass("is-active");
-	            }
-	            event.stopPropagation();
-	        });
-	        $("body").on("click", ".js-select-list li", function(event){
-	            var id = $(this).attr("data-id");
-	            var text = $(this).text();
-	            $(this).parents(".js-select").find(".js-select-text").text(text);
-	            $(this).parents(".js-select").find(".js-select-input").val(id);
-	            $(this).parent().hide();
-	            $(this).parents(".js-select").removeClass("is-active");
-	            event.stopPropagation();
-	        });
-	    }  
-	    
-	    selectList();
-	    $("body").on("click", ".js-select", function(event){
-	        event.stopPropagation();
-	    });
-	    
-	});
-	//lang
-	$('.lang__ru').on('click', function() {
-		if ($('.lang__eng').hasClass('is-close')) {
-			$('.lang__eng').removeClass('is-close');
-		}
-		else {
-			$('.lang__eng').addClass('is-close');
-			$(this).removeClass('is-close');
-		};
-		$('.lang').toggleClass('is-active');
-		return false;
-	});
-	$('.lang__eng').on('click', function() {
-		if ($('.lang__eng').hasClass('is-close')) {
-			$('.lang__ru').addClass('is-close');
-		}
-		else {
-			$('.lang__ru').addClass('is-close');
-		};
-		$('.lang').toggleClass('is-active');
-		return false;
-	});
-	//tabs
-	$('.form__tab').click(function(event) {
-		/* Act on the event */
-		$('.form__tab').removeClass('is-active');
-		$(this).addClass('is-active');
-		attr = $(this).attr('href');
-		$('#transfer,#follow,#add').removeClass('is-active');
-		$(' '+attr).addClass('is-active');
-		return false;
-	});
-	//input mask
-	$('.input__phone').inputmask("+9 (999) 999999999"); //mask with dynamic syntax 
-	$('.input__card').inputmask("9999 9999 9999 9999"); 
-	//
+
+	//spinner
 	$(function() {
 	  var spinner = $( ".input__spinner" ).spinner({ min: 0, max: 100});
-	
+
 	  $( "#disable" ).click(function() {
 	    if ( spinner.spinner( "option", "disabled" ) ) {
 	      spinner.spinner( "enable" );
@@ -141,28 +77,216 @@ head.ready(function() {
 	  $( "#setvalue" ).click(function() {
 	    spinner.spinner( "value", 5 );
 	  });
-	
+
 	  $( "button" ).button();
+
+	  var day = $( ".input_day" ).spinner({ min: 01, max: 31 });
+	  var year = $( ".input_year" ).spinner({ min: 2014, max: 9999})
 	});
-	//NAVIGATION
-	$('.nav-mob').on('click', function() {
-		$('.nav').toggleClass('is-active');
+
+	//nav
+	$('.nav_btn').on('click', function(event) {
+		$('.nav').toggleClass('is-open');
+		$(this).toggleClass('is-active');
 	});
-	//btn-next
-	$('.head-next').click(function() {
-		$('html, body').animate({scrollTop: $('.transfers').offset().top}, 400);
-		return false;
-	});
-	$('.transfers-next').click(function() {
-		$('html, body').animate({scrollTop: $('.partners').offset().top}, 400);
-		return false;
-	});
-	$('.partners-next').click(function() {
-		$('html, body').animate({scrollTop: $('.news').offset().top}, 400);
-		return false;
-	});
-	$('.btn-up').click(function() {
-		$('html, body').animate({scrollTop: $('html').offset().top}, 700);
-		return false;
-	});
+
+	//validation
+	$.validator.addMethod(
+		"regex",
+		function(value, element, regexp) {
+			var re = new RegExp(regexp);
+			return this.optional(element) || re.test(value);
+		},
+		"Данные не соответствуют шаблону"
+	);
 });
+
+function initPaymentForm(selector, validation_rules, validation_messages)
+{
+	return initForm(selector, validation_rules, validation_messages, function(form)
+	{
+		var $form = $(form);
+		var amount = $form.find('input[name=amount]').val();
+		var $submit_btn = $form.find(":submit");
+		$submit_btn.attr('disabled', 'disabled');
+		$.ajax({
+			type: $form.attr('method'),
+			url: $form.attr('action') || $form.data('action'),
+			data: $form.serialize(),
+			dataType: 'json'
+		}).success(function (json)
+		{
+			ga('ec:setAction', 'purchase', {
+                'step': 3,
+                'id': json.data.id,
+                'revenue': json.data.total
+            });
+            ga('send', 'event', 'Ecommerce', 'purchase');
+            var payment_url = json.data.card.payment_page_url;
+            countdown('#ipsp_popup', function () {
+                window.location.href = payment_url;
+            });
+		}).fail(function (jqXHR)
+		{
+            ga('send', 'event', 'failure', 'create_payment_failure', amount);
+            var json = jqXHR.responseJSON;
+            if(500 == json.meta.code)
+            {
+                $('.error_popup').show();
+            }
+            else
+            {
+                var messages = json.meta.error_message;
+                showFormErrors($submit_btn, $form, messages);
+            }
+            $submit_btn.removeAttr('disabled');
+		});
+		return false;
+	});
+}
+
+function showErrorPopup()
+{
+    $('.popup-container').hide();
+    $('.error_popup').show();
+}
+
+function hideErrorPopup()
+{
+    $('.error_popup').hide();
+}
+
+function initForm(selector, validation_rules, validation_messages, submit_callback)
+{
+	var $form = $(selector);
+
+    $form.find(':input').change(function() {
+        ga('ec:setAction', 'checkout', { 'step': 2 });
+        ga('send', 'event', 'Ecommerce', 'checkout');
+    });
+
+	return $form.validate({
+		onfocusout: false,
+		showErrors: function (errorMap) {
+			showJqueryValidatorErrors($form, errorMap);
+		},
+		rules: validation_rules,
+		messages: validation_messages,
+		submitHandler: submit_callback
+	});
+}
+
+function countdown(popup_selector, callback)
+{
+	$(popup_selector).show();
+    setTimeout(function() {
+        $(popup_selector).hide();
+        return callback();
+    }, 1000);
+}
+
+function showJqueryValidatorErrors($form, errorMap)
+{
+	$form.find(':input').each(function (i) {
+		var $input = $(this);
+		var property_name = $input.attr('name');
+		var $wrap = $input.parent();
+		var $msgbox = $wrap.find('.input-error');
+
+		if(undefined == property_name)
+			return;
+
+		if (errorMap.hasOwnProperty(property_name)) {
+			$msgbox.html(errorMap[property_name]);
+			$wrap.addClass('error');
+		}
+		else {
+			$msgbox.html('');
+			$wrap.removeClass('error');
+		}
+
+        ga('send', 'event', {
+            'eventCategory': 'Payment',
+            'eventAction': 'ValidationError',
+            'eventLabel': $form.data('action') + ':' + property_name
+        });
+	});
+}
+
+function showFormErrors($submit_btn, $form, messages)
+{
+	$submit_btn.removeAttr('disabled');
+	if(messages instanceof Array)
+	{
+		$.each(messages, function (field, message) {
+			var $el = $('*[name="' + field + '"]');
+			$el.addClass('error');
+		});
+	}
+	else
+	{
+		$form.find('.form-error').html(messages);
+	}
+}
+
+function initJSLists()
+{
+    $(document).click(function()
+    {
+        $(".js-select-list").hide();
+        $(".js-select").removeClass("is-active");
+    });
+
+    function selectList()
+    {
+        var select = $(".js-select");
+        var select_list = $(".js-select-list");
+        $("body").on("click", ".js-select", function(event){
+            if ($(this).hasClass("is-active")) {
+                select.removeClass("is-active");
+                select_list.hide();
+            }
+            else {
+                select.removeClass("is-active");
+                select_list.hide();
+                $(this).find(".js-select-list").show();
+                $(this).addClass("is-active");
+            }
+            event.stopPropagation();
+        });
+        $("body").on("click", ".js-select-list li", function(event){
+            var id = $(this).attr("data-id");
+            var text = $(this).text();
+            $(this).parents(".js-select").find(".js-select-text").text(text);
+            $(this).parents(".js-select").find(".js-select-input").val(id);
+            $(this).parent().hide();
+            $(this).parents(".js-select").removeClass("is-active");
+            $(this).parents(".js-select").find(".js-select-text").addClass("is-active");
+            $(this).parents(".js-select").find(".js-select-input").trigger('change');
+            event.stopPropagation();
+        });
+    }
+
+    function combinedSelectList()
+    {
+        $("body").on("click", ".js-combined-select .js-select-list li", function(event)
+        {
+            var $combined_el = $(this).parents(".js-combined-select");
+            var value = $combined_el.find('.js-select input[type=hidden]')
+                .map(function() {
+                    return $(this).val();
+                }).toArray().join('');
+
+            $combined_el.find('.js-combined-select-input').val(value);
+            event.stopPropagation();
+        });
+    }
+
+    selectList();
+    combinedSelectList();
+    $("body").on("click", ".js-select", function(event) {
+        event.stopPropagation();
+    });
+}
+
+
